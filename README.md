70144aa8767f9b009aa56222e2bffd325366e269
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2018-12-01 22:17:17 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # gfx
-a collection of handy animation functions
+
+A collection of handy GFX and Animation functions
+
+## Example
+
+https://tjb-webcomponents.github.io/tjb-gfx/
+
+## Components
+
+### Bounce
+
+Google style circle placed at some elements that grows in size from there:  
+[![Bounce Example](/gifs/bounce.gif)](https://tjb-webcomponents.github.io/tjb-gfx/#bounce)
+
+#### Add to project
+
+##### Include via HTML
+
+Include it:
+
+```JavaScript
+import { bounce } from 'https://tjb-webcomponents.github.io/tjb-gfx/tjb-gfx.min.js'
+```
+
+##### Include via NPM
+
+Console:
+
+```bash
+npm i -S tjb-gfx-bounce
+```
+
+Then in your code:
+
+```JavaScript
+import { bounce } from 'tjb-gfx-bounce';
+```
+
+#### Useage
+
+```JavaScript
+/**
+ * Creates a circle that fills the entire div
+ * Calls a callback after the animation
+ * @param {node} element
+ * @param {function} callback
+ * @param {boolean} cleanup whether or not to remove elements after animation
+ */
+bounce(element, callback, cleanup);
+```
+
+# Enjoy
+
+[![Typewriter Gif](https://tjb-webcomponents.github.io/html-template-string/typewriter.gif)](http://thibaultjanbeyer.com/)
---
+++ tjb-gfx.js
@@ -0,0 +1,45 @@
+import html from "https://tjb-webcomponents.github.io/html-template-string/html-template-string.js";
+
+/**
+ * Creates a circle that fills the entire div
+ * Calls a callback after the animation
+ * @param {node} element
+ * @param {function} callback
+ * @param {boolean} cleanup whether or not to remove elements after animation
+ */
+export function bounce(element, callback, cleanup) {
+  const bounceStyles = html `
+    <style>
+      .tjb-bounce {
+        top: 50%;
+        left: 50%;
+        width: 5px;
+        height: 5px;
+        position: absolute;
+        background-color: #1a2980;
+        border-radius: 50%;
+        z-index: 1000;
+        transform: translate(-50%, -50%) scale(1, 1);
+        animation: tjb-bounce 600ms linear 1 forwards;
+      }
+
+      @keyframes tjb-bounce {
+        100% {
+          background-color: #fff;
+          transform: translate(-50%, -50%) scale(800, 800);
+        }
+      }
+    </style>
+  `;
+
+  const bounce = document.createElement("span");
+  bounce.appendChild(bounceStyles);
+  element.style.position = "relative";
+  element.appendChild(bounce);
+  bounce.classList.add("tjb-bounce")
+  bounce.addEventListener("animationend", e => {
+    callback(e);
+    if (cleanup)
+      setTimeout(() => element.removeChild(bounce), 1000);
+  });
+}
